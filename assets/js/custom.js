6b5c0fc44b34aa04eba6940eb99f92a4aea98936
mazharul90007 <mazharul90007@gmail.com>
2024-08-07 14:11:06 +0600

    random keyboard on the game screen function added and the alphabet connected with the keyboard backgroun color change function added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,8 +7,23 @@
 //     const playgroundSection = document.getElementById("play-ground");
 //     playgroundSection.classList.remove("hidden");
 // }
+function continueGame(){
+    // step 1 - generate a random alphabet
+    const alphabet = getRandomAlphabet();
+
+    // set randomly generated alphabet to the screen
+    const currentAlphabetElement = document.getElementById("current-alphabet");
+    currentAlphabetElement.innerText = alphabet;
+
+    // set background color
+    setBackgroundColorById(alphabet);
+
+
+}
+
 
 function play(){
     hideElementById("home-screen");
     showElementById("play-ground");
+    continueGame();
 }
